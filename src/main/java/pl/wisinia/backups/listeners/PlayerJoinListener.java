bbc0246e79e589d64e00c699a75022b308fe47ed
Tomasz Gregor <127117177+tomcio09@users.noreply.github.com>
2026-07-16 19:13:03 +0200

    Update PlayerJoinListener.java

--- src/main/java/pl/wisinia/backups/listeners/PlayerJoinListener.java
+++ src/main/java/pl/wisinia/backups/listeners/PlayerJoinListener.java
@@ -1,14 +1,21 @@
 package pl.wisinia.backups.listeners;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.kyori.adventure.title.Title;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.scheduler.BukkitRunnable;
 import pl.wisinia.backups.WisiniaBackups;
 
+import java.time.Duration;
+
 public class PlayerJoinListener implements Listener {
+
+    private static final LegacyComponentSerializer LEGACY = LegacyComponentSerializer.legacyAmpersand();
 
     private final WisiniaBackups plugin;
 
@@ -16,31 +23,30 @@ public class PlayerJoinListener implements Listener {
         this.plugin = plugin;
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
-        if (plugin.getDataManager().hasPendingBackup(player.getUniqueId())) {
-            // Opóźnij powiadomienie o 2 sekundy po zalogowaniu
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    if (player.isOnline()) {
-                        // Subtitle jako action bar + title
-                        player.sendTitle(
-                                "",
-                                LegacyComponentSerializer.legacyAmpersand()
-                                        .deserialize("&7Masz &abackupa &7do odebrania pod &f/odbierzbackup&7!")
-                                        .content(),
-                                10, 80, 20
-                        );
+        // Cache nick -> UUID
+        plugin.getDataManager().cachePlayer(player.getName(), player.getUniqueId());
 
-                        // Wyślij również na czat dla pewności
-                        player.sendMessage(LegacyComponentSerializer.legacyAmpersand()
-                                .deserialize("&7Masz &abackupa &7do odebrania pod &f/odbierzbackup&7!"));
-                    }
-                }
-            }.runTaskLater(plugin, 40L);
+        if (plugin.getDataManager().hasPendingBackup(player.getUniqueId())) {
+            // Opóźnij powiadomienie o 3 sekundy
+            plugin.getServer().getScheduler().runTaskLater(plugin, () -> {
+                if (!player.isOnline()) return;
+
+                Component subtitle = LEGACY.deserialize(
+                        "&7Masz &abackupa &7do odebrania pod &f/odbierzbackup&7!"
+                ).decoration(TextDecoration.ITALIC, false);
+
+                Title.Times times = Title.Times.times(
+                        Duration.ofMillis(500),
+                        Duration.ofSeconds(4),
+                        Duration.ofMillis(1000)
+                );
+
+                player.showTitle(Title.title(Component.empty(), subtitle, times));
+            }, 60L);
         }
     }
 }
